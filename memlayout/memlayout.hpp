035491f9085b50f4713ea098753fb081cf9495e5
Ailrk <jimmy123good@hotmail.com>
2021-11-18 18:18:23 -0800

    stage 1

--- memlayout/memlayout.hpp
+++ memlayout/memlayout.hpp
@@ -55,6 +55,10 @@ M_CEXPR decltype(auto) wrap_op(auto op, size_t n, auto x, auto y) noexcept {
     static_assert(
         std::is_same_v<decltype(x), decltype(y)>,
         "wrapping arithmetics are required to perform on the same type");
+    static_assert(
+        std::is_unsigned_v<decltype(x)>,
+        "wrapping arithmetics are required to perform on the same type");
+
     return op(x, y) % n;
 }
 
@@ -132,8 +136,8 @@ class Layout {
         : size_(size)
         , align_(align) {}
 
-    M_CEXPR static std::optional<Layout> from_size_align(size_t size,
-                                                         size_t align) {
+    M_CEXPR static std::optional<Layout>
+    from_size_align(size_t size, size_t align) noexcept {
         if (align != 0 && is_power_of_two(align))
             return {};
         if (size > std::numeric_limits<size_t>::max() - (align - 1))
@@ -146,14 +150,23 @@ class Layout {
         : size_(0)
         , align_(0) {}
 
+    // create a new layout at compile time.
+    template <typename T>
+    M_CEXPR static std::optional<Layout> create() noexcept {
+        auto [size, align] = size_align<T>();
+        return from_size_align(size, align);
+    }
+
     M_CEXPR size_t size() const noexcept { return size_; }
     M_CEXPR size_t align() const noexcept { return align_; }
 
     // create an aligned dangling pointer
-    inline char *aligned_dangling_ptr() { return (char *)align(); }
+    inline char *aligned_dangling_ptr() const noexcept {
+        return (char *)align();
+    }
 
     // align to `align` if  this.align is not already aligned.
-    M_CEXPR std::optional<Layout> align_to(size_t align) {
+    M_CEXPR std::optional<Layout> align_to(size_t align) const noexcept {
         return { Layout::from_size_align(size(),
                                          std::max(this->align(), align)) };
     }
@@ -162,7 +175,7 @@ class Layout {
     // rounded_up_size = (size + align - 1) & !(align - 1)
     // where (align - 1) ensures when overflow rounded up size is 0.
     // required_padding = rounded_up_size - size.
-    M_CEXPR size_t required_padding(size_t align) {
+    M_CEXPR size_t required_padding(size_t align) const noexcept {
         auto size = this->size();
         auto rounded_up_size = wrap_sub(wrap_add(size, align), (size_t)1) &
                                !wrap_sub(align, (size_t)1);
@@ -170,13 +183,13 @@ class Layout {
     }
 
     // return layout that has size added with padding for given alignment.
-    M_CEXPR Layout pad_to_align() {
+    M_CEXPR Layout pad_to_align() noexcept {
         auto new_size = required_padding(align()) + size();
         return Layout::from_size_align(new_size, align()).value();
     }
 
     // repeat Layout n times with padding in between.
-    M_CEXPR std::optional<std::pair<Layout, size_t>> repeat(size_t n) {
+    M_CEXPR std::optional<std::pair<Layout, size_t>> repeat(size_t n) noexcept {
         size_t padded_size = size() + required_padding(align());
         if (auto allocate_size = checked_mul(padded_size, n)) {
             return { { Layout::from_size_align(allocate_size.value(), align())
@@ -187,7 +200,7 @@ class Layout {
     }
 
     // repeat Layout n times without adding padding.
-    M_CEXPR std::optional<Layout> repeat_packed(size_t n) {
+    M_CEXPR std::optional<Layout> repeat_packed(size_t n) noexcept {
         if (auto sz = checked_mul(size(), n)) {
             if (auto layout = Layout::from_size_align(sz.value(), align())) {
                 return layout;
@@ -197,7 +210,8 @@ class Layout {
     }
 
     // extend layout A with layout B, adding proper padding.
-    M_CEXPR std::optional<std::pair<Layout, size_t>> extend(Layout after) {
+    M_CEXPR std::optional<std::pair<Layout, size_t>>
+    extend(Layout after) noexcept {
         size_t new_align = std::max(align(), after.align());
         size_t padding = required_padding(new_align);
 
@@ -214,7 +228,7 @@ class Layout {
     }
 
     // extend with the same aligment.
-    M_CEXPR std::optional<Layout> extend_packed(Layout after) {
+    M_CEXPR std::optional<Layout> extend_packed(Layout after) noexcept {
         if (auto new_size = checked_add(size(), after.size())) {
             return Layout::from_size_align(new_size.value(), align());
         }
@@ -222,7 +236,8 @@ class Layout {
     }
 
     // layout for std::array<n, T>
-    template <typename T> M_CEXPR std::optional<Layout> array(size_t n) {
+    template <typename T>
+    M_CEXPR std::optional<Layout> array(size_t n) noexcept {
         if (auto layout = Layout::create<T>()) {
             if (auto p = layout.value().repeat(n)) {
                 auto [layout, offset] = p;
@@ -232,13 +247,7 @@ class Layout {
         return {};
     }
 
-    // create a new layout at compile time.
-    template <typename T> M_CEXPR static std::optional<Layout> create() {
-        auto [size, align] = size_align<T>();
-        return from_size_align(size, align);
-    }
-
-    friend inline std::string to_string(const Layout &self) {
+    friend inline std::string to_string(const Layout &self) noexcept {
         return "<Layout| size:" + std::to_string(self.size()) +
                ", align: " + std::to_string(self.align()) + ">";
     }
@@ -266,21 +275,21 @@ template <typename T> class HasLayout {
         , layout_(Layout::create<T>().value()) {}
 
   public:
-    M_CEXPR static std::optional<HasLayout<T>> create(T *value) {
+    M_CEXPR static std::optional<HasLayout<T>> create(T *value) noexcept {
         if (value == nullptr) {
             return {};
         }
-        return { HasLayout<T>(std::forward<std::remove_cvref_t<T> *>(value)) };
+        return { HasLayout<T>(std::forward<T *>(value)) };
     }
 
     M_CEXPR T *ptr() { return value_; }
-    M_CEXPR std::optional<std::reference_wrapper<T>> deref() {
+    M_CEXPR std::optional<std::reference_wrapper<T>> deref() noexcept {
         if (value_ == nullptr) {
             return {};
         }
         return { *value_ };
     }
-    M_CEXPR Layout layout() { return layout_; }
+    M_CEXPR Layout layout() noexcept { return layout_; }
 };
 
 //
@@ -298,11 +307,12 @@ class SomeHasLayout {
         , layout_(Layout::create<T>()) {}
 
   public:
-    template <typename T> static std::optional<SomeHasLayout> create(T *value) {
+    template <typename T>
+    static std::optional<SomeHasLayout> create(T *value) noexcept {
         if (value == nullptr) {
             return {};
         }
-        return { SomeHasLayout(std::forward<std::remove_cvref_t<T> *>(value)) };
+        return { SomeHasLayout(std::forward<T *>(value)) };
     }
 
     M_CEXPR void *ptr() { return value_; }
@@ -310,14 +320,16 @@ class SomeHasLayout {
 };
 
 // smart constructor for HasLayout.
-template <typename T> std::optional<HasLayout<T>> make_has_layout(T &&value) {
-    return HasLayout<T>::create(std::forward<T &&>(value));
+template <typename T>
+std::optional<HasLayout<T>> make_has_layout(T *value) noexcept {
+    return HasLayout<std::remove_cvref_t<T>>::create(std::forward<T *>(value));
 }
 
 // smart constructor for HasSomeLayout.
 template <typename T>
-std::optional<SomeHasLayout> make_some_has_layout(T &&value) {
-    if (auto v1 = HasLayout<T>::create(std::forward<T &&>(value))) {
+std::optional<SomeHasLayout> make_some_has_layout(T *value) noexcept {
+    if (auto v1 = HasLayout<std::remove_cvref_t<T>>::create(
+            std::forward<T *>(value))) {
         return { SomeHasLayout(std::move(v1.value())) };
     }
     return {};
